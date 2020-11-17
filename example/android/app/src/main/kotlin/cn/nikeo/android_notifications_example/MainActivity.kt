3a35baca292585d41d94795b48062f9748d99c6c
Lenox Xian <nikeorever@gmail.com>
2020-11-17 18:53:15 +0800

    Optimize

--- example/android/app/src/main/kotlin/cn/nikeo/android_notifications_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/cn/nikeo/android_notifications_example/MainActivity.kt
@@ -5,10 +5,4 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
 class MainActivity : FlutterActivity() {
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        cn.nikeo.android_notifications_example.R.mipmap.ic_launcher
-//        flutterEngine.plugins.add(AndroidNotificationsPlugin(R.mipmap.ic_launcher))
-    }
 }
